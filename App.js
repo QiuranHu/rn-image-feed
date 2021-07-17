edcca6ffb13c8dfd6ab072290501c0ae9b4bcd68
Qiuran Hu <qiuranh@gmail.com>
2021-07-17 08:32:28 +0800

    checkin

--- App.js
+++ App.js
@@ -1,21 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
+import React from "react";
+import {
+  Modal,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  AsyncStorage,
+} from "react-native";
+import CardList from "./components/CardList";
+import Feed from "./screens/Feed";
+import Comments from "./screens/Comments";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const items = [
+  { id: 0, author: "Bob Ross" },
+  { id: 1, author: "Chuck Norris" },
+];
+
+const ASYNC_STORAGE_COMMENTS_KEY = "ASYNC_STORAGE_COMMENTS_KEY";
+
+export default class App extends React.Component {
+  async componentDidMount() {
+    try {
+      const commentsForItem = await AsyncStorage.getItem(
+        ASYNC_STORAGE_COMMENTS_KEY
+      );
+      this.setState({
+        commentsForItem: commentsForItem ? JSON.parse(commentsForItem) : {},
+      });
+    } catch (e) {
+      console.log("Failed to load comments");
+    }
+  }
+  state = {
+    commentsForItem: {},
+    showModal: false,
+    selectedItemId: null,
+  };
+
+  openCommentScreen = (id) => {
+    this.setState({
+      showModal: true,
+      selectedItemId: id,
+    });
+  };
+
+  closeCommentScreen = () => {
+    this.setState({
+      showModal: false,
+      selectedItemId: null,
+    });
+  };
+
+  onSubmitComment = (text) => {
+    const { selectedItemId, commentsForItem } = this.state;
+    const comments = commentsForItem[selectedItemId] || [];
+    const updated = {
+      ...commentsForItem,
+      [selectedItemId]: [...comments, text],
+    };
+    this.setState({ commentsForItem: updated });
+    try {
+      AsyncStorage.setItem(ASYNC_STORAGE_COMMENTS_KEY, JSON.stringify(updated));
+    } catch (e) {
+      console.log("Failed to save comment", text, "for", selectedItemId);
+    }
+  };
+
+  render() {
+    const { commentsForItem, showModal, selectedItemId } = this.state;
+    return (
+      <View style={styles.container}>
+        <Feed
+          style={styles.feed}
+          commentsForItem={commentsForItem}
+          onPressComments={this.openCommentScreen}
+        />
+        <Modal
+          visible={showModal}
+          animationType="slide"
+          onRequestClose={this.closeCommentScreen}
+        >
+          <Comments
+            style={styles.comments}
+            comments={commentsForItem[selectedItemId] || []}
+            onClose={this.closeCommentScreen}
+            onSubmitComment={this.onSubmitComment}
+          />
+        </Modal>
+      </View>
+    );
+  }
 }
+
+const platformVersion =
+  Platform.OS === "ios" ? parseInt(Platform.Version, 10) : Platform.Version;
 
 const styles = StyleSheet.create({
   container: {
+    // marginTop: Constants.statusBarHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // justifyContent: "flex-start",
+  },
+  feed: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "android" || platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
+  },
+  comments: {
+    flex: 1,
+    marginTop:
+      Platform.OS === "ios" && platformVersion < 11
+        ? Constants.statusBarHeight
+        : 0,
   },
 });
